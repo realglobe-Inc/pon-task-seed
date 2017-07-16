c9bd187f17128f4a0d38d87f6daf585e47067f04
Taka Okunishi <okunishinishi@gmail.com>
2017-07-17 03:02:22 +0900

    Update deps

--- test/define_test.js
+++ test/define_test.js
@@ -8,28 +8,27 @@ const define = require('../lib/define.js')
 const theDB = require('the-db')
 const ponContext = require('pon-context')
 const { ok } = require('assert')
-const co = require('co')
 
 describe('define', function () {
   this.timeout(3000)
 
-  before(() => co(function * () {
+  before(async () => {
 
-  }))
+  })
 
-  after(() => co(function * () {
+  after(async () => {
 
-  }))
+  })
 
-  it('Define', () => co(function * () {
+  it('Define', async () => {
     let db = theDB({})
     let ctx = ponContext()
     let pattern = `${__dirname}/../misc/mocks/:env/*.seed.json`
     let task = define(() => db, pattern)
     ok(task)
 
-    yield Promise.resolve(task(ctx))
-  }))
+    await Promise.resolve(task(ctx))
+  })
 })
 
 /* global describe, before, after, it */
